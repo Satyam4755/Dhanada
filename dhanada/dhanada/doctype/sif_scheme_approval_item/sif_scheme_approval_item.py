cc5890fc81a5ab92271f30592722218ce7d7299a
Satyam Raj <smritisoni@DODO-20.local>
2026-08-05 14:39:37 +0530

    Remove unused utility scripts and duplicate doctypes

--- dhanada/dhanada/doctype/sif_scheme_approval_item/sif_scheme_approval_item.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, KNAPS Private Limited and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class SIFSchemeApprovalItem(Document):
-	_DOCTYPE_NAME = "SIF Scheme Approval Item"
--- dhanada/fetch_interest.py
+++
@@ -1,5 +0,0 @@
-import frappe
-import json
-
-def run():
-    print(json.dumps(frappe.db.get_value('Custom Field', 'CRM Lead-interest', ['in_list_view', 'in_standard_filter'], as_dict=True)))
